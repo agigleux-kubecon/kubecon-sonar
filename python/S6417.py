40ce7922a307fb328f1d8016e4e48d08adca753f
agigleux <alexandre.gigleux@sonarsource.com>
2022-10-03 14:38:41 +0200

    Update S6417

--- python/S6417.py
+++ python/S6417.py
@@ -1,38 +1,22 @@
-def my_fun(my_dict: dict[str, int]):
-    for key in my_dict:
-        if key == 'foo':
-            my_dict.pop(key) # Noncompliant
-
-def get_dict():
-    return {'foo': 1, 'bar': 2}
-
-my_fun(get_dict())         
-
-def my_fun2():
+def my_fun_inside_function():
     my_dict = {'foo': 1, 'bar': 2}
     for key in my_dict:
         if key == 'foo':
             my_dict.pop(key) # Noncompliant
-
-def my_fun3():
-    my_dict = get_dict()
-    for key in my_dict:
-        if key == 'foo':
-            my_dict.pop(key) # Noncompliant
-
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-for key in my_dict:
-    print(key)
-    if key == 'b':
-        my_dict.pop(key) # Noncompliant            
-
-
+       
 def create_dict():
    return {'a': 1, 'b': 2, 'c': 3}
 
 def dict_retrieved_from_function_call():
-    my_dict_from_function_call = create_dict()
-    for key in my_dict_from_function_call:
+    my_dict = create_dict()
+    for key in my_dict:
         print(key)
         if key == 'b':
-            my_dict_from_function_call.update(key, 0) # Noncompliant       
+            my_dict.update(key, 0) # Noncompliant       
+
+def my_fun_as_parameter(my_dict: dict[str, int]):
+    for key in my_dict:
+        if key == 'b':
+            my_dict.pop(key) # Noncompliant (soon)
+
+my_fun_as_parameter(create_dict())     
